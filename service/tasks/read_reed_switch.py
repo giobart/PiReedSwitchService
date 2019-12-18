fa5ebca55c96283ccdc8aa4d47bcbfd1983ec9ff
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2019-12-18 15:51:27 +0100

    added sensor log

--- service/tasks/read_reed_switch.py
+++ service/tasks/read_reed_switch.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from service.config import REED_OUT, REED_1_IN
 from service.config import NOTIFICATION_ENDPOINT
 import requests
+import logging
 
 time_loop = Timeloop()
 
@@ -17,15 +18,23 @@ GPIO.output(16, GPIO.HIGH)
 
 counter = 0
 
+
 @time_loop.job(interval=timedelta(seconds=1))
 def reed_switch_sensor():
-    input = GPIO.input(REED_1_IN)
+    reed_input = GPIO.input(REED_1_IN)
 
-    #send 1 notification every 10 input.
+    # used to send 1 notification every 10 input.
     global counter
 
-    if counter <= 0 and input == '1':
+    if counter <= 0 and reed_input == '1':
         requests.get(NOTIFICATION_ENDPOINT)
+        logging.info('DOOR SEEMS OPEN - NOTIFICATION SENT')
         counter = 10
-    elif input == '1':
+    elif reed_input == '1':
         counter -= 1
+
+
+@time_loop.job(interval=timedelta(seconds=1))
+def reed_switch_log():
+    reed_input = GPIO.input(REED_1_IN)
+    logging.info('Reed Switch Status: ' + reed_input)
